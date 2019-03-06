4c4645d54c2cd254a7587e427a78c24c7a2cde41
jun xie <iumnyo@163.com>
2019-03-06 17:43:23 +0800

    Initial Commit

---
+++ 新闻快速滚动栏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻快速滚动栏
+//
+//  Created by MacBook Pro on 2019/3/6.
+//  Copyright © 2019年 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
